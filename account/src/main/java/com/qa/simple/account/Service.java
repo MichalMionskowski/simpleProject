b7fb9ec97a6d8faa035edbbab1f7bb33af63b50b
michal.mionskowski <michal.mionskowski95@outlook.com>
2018-11-13 12:13:55 +0000

    object to json

--- account/src/main/java/com/qa/simple/account/Service.java
+++ account/src/main/java/com/qa/simple/account/Service.java
@@ -2,6 +2,9 @@ package com.qa.simple.account;
 
 import java.util.HashMap;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Service {
 	HashMap<Integer , Person> userList = new HashMap<Integer,Person>(); 
 	public static int AccountNumber = 0;
@@ -14,5 +17,12 @@ public class Service {
 	public void getFullName(int accountNumber) {
 			Person member = userList.get(accountNumber);
 			System.out.println(member.toString());
+	}
+	
+	public void printInJSON() throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		for(Person member : userList.values()) {
+			System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(member));
+		}
 	}
 }
