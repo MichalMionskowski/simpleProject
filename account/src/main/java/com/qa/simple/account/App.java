3456930f1bdd7de85eb5d8097958fe14e1befac7
michal.mionskowski <michal.mionskowski95@outlook.com>
2018-11-13 13:19:35 +0000

    fianl

--- account/src/main/java/com/qa/simple/account/App.java
+++ account/src/main/java/com/qa/simple/account/App.java
@@ -10,10 +10,6 @@ public class App
 {
     public static void main( String[] args ) throws JsonProcessingException
     {
-    	Service accounts = new Service();
-    	accounts.addAccount("Michal", "Mionskowski");
-    	accounts.addAccount("Vince", "one");
-    	accounts.addAccount("John", "two" );
-    	accounts.printInJSON();
+    	System.out.println("hello");
     }
 }
